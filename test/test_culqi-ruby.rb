632e7e8d7becb7dfbc30cd96b5717e97a081afdf
JordanCulqi <jordan.diaz@culqi.com>
2023-04-05 14:50:26 -0500

    se agrego yape y ordenes

--- test/test_culqi-ruby.rb
+++ test/test_culqi-ruby.rb
@@ -175,35 +175,31 @@ class CulqiTest < Minitest::Test
   # GET RESOURCES
 
   def test_get_token
-    assert_equal 'token', getToken['object']
+    assert_equal 'token', JSON.parse(Culqi::Token.get(getToken['id']))['object']
   end
 
   def test_get_charge
-    assert_equal 'charge', getCharge['object']
-  end
-
-  def test_get_order
-    assert_equal 'order', getOrder['object']
+    assert_equal 'charge', JSON.parse(Culqi::Charge.get(getCharge['id']))['object']
   end
 
   def test_get_plan
-    assert_equal "plan", getPlan['object']
+    assert_equal "plan", JSON.parse(Culqi::Plan.get(getPlan['id']))['object']
   end
 
   def test_get_customer
-    assert_equal 'customer', getCustomer['object']
+    assert_equal 'customer', JSON.parse(Culqi::Customer.get(getCustomer['id']))['object']
   end
 
   def test_get_card
-    assert_equal 'card', getCard['object']
+    assert_equal 'card', JSON.parse(Culqi::Card.get(getCard['id']))['object']
   end
 
   def test_get_subscription
-    assert_equal 'subscription', getSubscription['object']
+    assert_equal 'subscription', JSON.parse(Culqi::Subscription.get(getSubscription['id']))['object']
   end
 
   def test_get_refund
-    assert_equal 'refund', getRefund['object']
+    assert_equal 'refund', JSON.parse(Culqi::Refund.get(getRefund['id']))['object']
   end
 
   # DELETE RESOURCES
