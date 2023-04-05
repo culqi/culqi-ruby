a62fe1c8887fa3e221405f7a00a45a148d9173b3
JordanCulqi <jordan.diaz@culqi.com>
2023-04-05 13:14:46 -0500

    se agrego yape y ordenes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 require 'minitest/autorun'
 require 'culqi-ruby'
 
-Culqi.public_key = 'pk_live_da33560a681ff246'
-Culqi.secret_key = 'sk_live_93fd5e4babc0f7a6'
+Culqi.public_key = ''
+Culqi.secret_key = ''
