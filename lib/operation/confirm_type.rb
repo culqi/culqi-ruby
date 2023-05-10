de9a72f1c149d9aac929e3e3d91baa2ec433e794
JbyteSecurity <jordandiaz2016@gmail.com>
2023-05-10 18:21:32 -0500

    se agrego encriptacion a confirm

--- lib/operation/confirm_type.rb
+++ lib/operation/confirm_type.rb
@@ -6,10 +6,15 @@ module Culqi::ConfirmType
     @url = ''
   end
 
-  def confirm(params={})
+  def confirm(params={}, rsa_key='', rsa_id='')
     key = ''
+    if(rsa_key != '')
+      rsa_key = rsa_key
+      rsa_id = rsa_id
+      params = Encrypt.encrypt_with_aes_rsa(params, rsa_key, true)
+    end
     key = Culqi.public_key
-    response = Culqi.connect(@url+'confirm', key, params, 'post', Culqi::READ_TIMEOUT)
+    response = Culqi.connect(@url+'confirm', key, params, 'post', Culqi::READ_TIMEOUT, false, rsa_id)
     return response.read_body
   end
 
