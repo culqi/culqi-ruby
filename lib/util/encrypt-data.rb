8681064b07fc40609dfa7638c2c7e9bb23ca218f
JbyteSecurity <jordandiaz2016@gmail.com>
2023-07-27 21:23:21 -0500

    se agrego gcm

--- lib/util/encrypt-data.rb
+++ lib/util/encrypt-data.rb
@@ -1,7 +1,6 @@
 require 'openssl'
 require 'base64'
 require 'json'
-require 'openssl/oaep'
 
 module Encrypt
   def self.generate_random_bytes(length)
@@ -10,9 +9,9 @@ module Encrypt
 
   def self.encrypt_with_aes_rsa(data, public_key, is_json)
     key = generate_random_bytes(32) # Generate a 256-bit random key for AES encryption
-    iv = generate_random_bytes(16) # Generate a 128-bit random initialization vector for AES encryption
+    iv = generate_random_bytes(12)  # GCM mode requires a 96-bit (12 bytes) random initialization vector
 
-    cipher = OpenSSL::Cipher.new('AES-256-CBC')
+    cipher = OpenSSL::Cipher.new('AES-256-GCM')
     cipher.encrypt
     cipher.key = key
     cipher.iv = iv
@@ -25,22 +24,22 @@ module Encrypt
 
     encrypted_data = Base64.strict_encode64(encrypted)
 
-    ####
     rsa_public_key = OpenSSL::PKey::RSA.new(public_key)
     encrypted_key = rsa_encrypt(key, rsa_public_key)
     encrypted_iv = rsa_encrypt(iv, rsa_public_key)
 
-    { encrypted_data: encrypted_data, encrypted_key: encrypted_key, encrypted_iv: encrypted_iv }
+    # Get the authentication tag from the cipher after encryption
+    authentication_tag = cipher.auth_tag
+
+    { encrypted_data: encrypted_data, encrypted_key: encrypted_key, encrypted_iv: encrypted_iv, authentication_tag: Base64.strict_encode64(authentication_tag) }
   end
 
   def self.rsa_encrypt(data, public_key)
-    # Define the encryption parameters
     label = ''
 
     md = OpenSSL::Digest::SHA256
-    cipher_text = public_key.public_encrypt_oaep(data, label, md, md)
+    cipher_text = public_key.public_encrypt(data, OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING)
 
-    return Base64.strict_encode64(cipher_text)
+    Base64.strict_encode64(cipher_text)
   end
-
 end
