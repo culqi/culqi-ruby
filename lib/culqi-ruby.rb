1a4f70de6af3428d2369208fc5baa64fdc502922
Jose Calderon <jose.calderon@culqi.com>
2023-08-11 17:45:14 -0500

    ajustes

--- lib/culqi-ruby.rb
+++ lib/culqi-ruby.rb
@@ -20,10 +20,8 @@ require 'culqi/yape'
 require 'culqi/order'
 module Culqi
 
-  #API_BASE = 'https://api.culqi.com/v2'
-  #API_BASE_SECURE = 'https://secure.culqi.com/v2'
-  API_BASE = 'https://qa-api.culqi.xyz/v2'
-  API_BASE_SECURE = 'https://qa-secure.culqi.xyz/v2'
+  API_BASE = 'https://api.culqi.com/v2'
+  API_BASE_SECURE = 'https://secure.culqi.com/v2'
   READ_TIMEOUT = 120
   LIST_TIMEOUT = 360
 
